e57ffc47192ae1685aa73c5f1810b81b3af3027d
Mannat Garg <mannatgarg2006@gmail.com>
2026-01-20 13:47:39 +0530

    Updated code

---
+++ sampark/src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navbar from '@/components/Navbar';
+import HeroSection from '@/components/HeroSection';
+import ProblemSection from '@/components/ProblemSection';
+import SolutionSection from '@/components/SolutionSection';
+import FeaturesSection from '@/components/FeaturesSection';
+import GrievanceSection from '@/components/GrievanceSection';
+import ContactSection from '@/components/ContactSection';
+import Footer from '@/components/Footer';
+import Chatbot from '@/components/Chatbot';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <ProblemSection />
+      <SolutionSection />
+      <FeaturesSection />
+      <GrievanceSection />
+      <ContactSection />
+      <Footer />
+      <Chatbot />
+    </div>
+  );
+};
+
+export default Index;
